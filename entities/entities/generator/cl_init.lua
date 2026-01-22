80287e5bf9e3ba4dc8e84ca4b99ac8c5b2de67e2
DangerousMilk <galikgrisha@gmail.com>
2026-01-22 19:13:31 +0100

    Added light to the generator.

--- entities/entities/generator/cl_init.lua
+++ entities/entities/generator/cl_init.lua
@@ -1,6 +1,29 @@
 include("shared.lua")
 
--- Client-side draw function for the Entity
+function ENT:Think()
+    -- Light
+    local isOn = self:GetIsOn()
+    local light = DynamicLight(self:EntIndex())
+
+    -- Light position
+    local pos = self:GetPos()
+    pos.z = pos.z + 90
+
+    if ( light ) then
+	    light.pos = pos
+	    -- Color
+	    light.r = isOn and 50 or 255
+	    light.g = isOn and 255 or 50
+	    light.b = 50
+
+	    light.brightness = 1
+	    light.decay = 1
+	    light.size = 256
+	    light.dietime = CurTime() + 1
+    end
+end
+
 function ENT:Draw()
-    self:DrawModel() -- Draws the model of the Entity. This function is called every frame.
+    -- Draws the model of the Entity. This function is called every frame.
+    self:DrawModel()
 end
