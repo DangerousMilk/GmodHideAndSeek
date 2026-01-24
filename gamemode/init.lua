d055d6c60823804c2679f095ca8a833566ba92c1
DangerousMilk <galikgrisha@gmail.com>
2026-01-24 16:36:30 +0100

    Basic exit door logic.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -5,6 +5,8 @@ AddCSLuaFile("game_hud.lua")
 include("shared.lua")
 include("concommands.lua")
 
+door = nil
+
 function GM:PlayerSpawn(ply)
     ply:SetGravity(.80)
     ply:SetMaxHealth(100)
@@ -16,11 +18,33 @@ function GM:PlayerSpawn(ply)
     CreateConVar("hs_max_generators", "5", {FCVAR_ARCHIVE})
 
     SetGlobalInt("FixedGenerators", 0)
+
+    local doors = {}
+
+    for _, ent in ipairs(ents.GetAll()) do
+        if IsValid(ent) and (
+            ent:GetClass() == "prop_door_rotating" or
+            ent:GetClass() == "func_door" or
+            ent:GetClass() == "func_door_rotating"
+        ) then
+            table.insert(doors, ent)
+        end
+    end
+
+    if #doors == 0 then return nil end
+    door = table.Random(doors)
 end
 
 hook.Add("GeneratorFixed", "CheckGenerators", function()
     if GetGlobalInt("FixedGenerators") >= GetConVar("hs_max_generators"):GetInt() then
 	sound.Play("music/Ravenholm_1.mp3", Vector(0, 0, 0), 0, 100, 1)
 	print("Niga")
+    end
+end)
+
+hook.Add("PlayerUse", "TrackExitDoor", function(ply, ent)
+
+    if ent == door then
+        print(ply:Nick(), "used the random door")
     end
 end)
